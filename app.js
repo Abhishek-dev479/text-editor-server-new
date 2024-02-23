af4acecd8408e6de1b4e7e03ff7cdab743686fcd
abhi <chunduriabhi@gmail.com>
2024-02-23 17:23:06 +0530

    new commit

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({origin: '*'}));
+app.options('*', cors())
 
 function getDate(){
     const currentDate = new Date();
